431d8b0e448e92ea84d755cba1471c14988dc686
Benjamin Pannell <benjamin@pannell.dev>
2021-11-12 13:58:44 +0000

    fix: Don't define the main method in __init__.py

--- lunchmoney_automate/__main__.py
+++ lunchmoney_automate/__main__.py
@@ -1,4 +1,29 @@
-from lunchmoney_automate import main
+import datetime
+import logging
+import os
+import json
+from typing import List
+
+from .task import Task
+
+from .link_transfers import LinkTransfersTask
+from .link_spare_change import LinkSpareChangeTask
+
+
+def main() -> None:
+    logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO"))
+
+    config = json.loads(os.getenv("LUNCHMONEY_CONFIG", "{}"))
+
+    tasks: List[Task] = []
+    if "transfers" in config:
+        tasks.append(LinkTransfersTask(**config["transfers"]))
+
+    if "spare_change" in config:
+        tasks.extend([LinkSpareChangeTask(**item) for item in config["spare_change"]])
+
+    for task in tasks:
+        task.run()
 
 if __name__ == '__main__':
     main()
