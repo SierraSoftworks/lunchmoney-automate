00fe3ac4feb538696d91ac03c5d45bd668545d71
Benjamin Pannell <benjamin@pannell.dev>
2021-11-12 14:05:45 +0000

    fix: Ensure that we can run the lunchmoney automation in GitHub CI

--- main.py
+++ main.py
@@ -4,24 +4,28 @@ import os
 import json
 from typing import List
 
-from .task import Task
+from lunchmoney_automate.task import Task
 
-from .link_transfers import LinkTransfersTask
-from .link_spare_change import LinkSpareChangeTask
+from lunchmoney_automate.link_transfers import LinkTransfersTask
+from lunchmoney_automate.link_spare_change import LinkSpareChangeTask
 
 
 def main() -> None:
     logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO"))
 
+    logging.info("Loading configuration...")
     config = json.loads(os.getenv("LUNCHMONEY_CONFIG", "{}"))
 
     tasks: List[Task] = []
     if "transfers" in config:
+        logging.info("Link Transfers task enabled in configuration")
         tasks.append(LinkTransfersTask(**config["transfers"]))
 
     if "spare_change" in config:
+        logging.info("Link Spare Change task enabled in configuration")
         tasks.extend([LinkSpareChangeTask(**item) for item in config["spare_change"]])
 
+    logging.info("Running tasks...")
     for task in tasks:
         task.run()
 
