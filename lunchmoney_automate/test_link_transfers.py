b852ff58622367c608b76f479754cd925bfeaa64
Benjamin Pannell <benjamin@pannell.dev>
2021-11-12 13:52:34 +0000

    fix: Correctly name a few things to make sure they run correctly

--- lunchmoney_automate/test_link_transfers.py
+++ lunchmoney_automate/test_link_transfers.py
@@ -1,12 +1,12 @@
 from unittest.mock import patch
 
-from .link_spare_change import LinkSpareChangeTask
+from .link_transfers import LinkTransfersTask
 
 
-def test_link_spare_change(call_lunchmoney):
-    task = LinkSpareChangeTask('Test Asset 2', 'Test Asset 1')
-    
-    with patch('HourlyAutomation.link_spare_change.call_lunchmoney', side_effect=call_lunchmoney) as lunchmoney_mock:
+def test_link_transactions(call_lunchmoney):
+    task = LinkTransfersTask(create_if_missing=False)
+
+    with patch('lunchmoney_automate.link_transfers.call_lunchmoney', side_effect=call_lunchmoney) as lunchmoney_mock:
         task.run()
 
         for call in lunchmoney_mock.call_args_list:
@@ -16,16 +16,16 @@ def test_link_spare_change(call_lunchmoney):
         lunchmoney_mock.assert_any_call('GET', '/v1/plaid_accounts')
         lunchmoney_mock.assert_any_call('GET', '/v1/categories')
         lunchmoney_mock.assert_any_call('GET', '/v1/transactions', params={
-            'asset_id': 72,
+            'category_id': 85,
             'start_date': task.start_date,
             'end_date': task.end_date,
             'is_group': "false"
         })
         lunchmoney_mock.assert_any_call('POST', '/v1/transactions/group', json={
             'date': '2020-01-02',
-            'payee': 'Walmart',
-            'category_id': 83,
-            'notes': None,
+            'payee': 'Test Asset 2 to Test Asset 1',
+            'category_id': 85,
+            'notes': 'USD 100.0000',
             'tags': [],
-            'transactions': [603, 606, 607]
-        })
+            'transactions': [605, 604]
+    })
